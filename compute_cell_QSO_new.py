4e9f217b17cd2eda4cd261dacaff0d4b588a16c7
Sofia Chiarenza <sofia.chiarenza@studenti.unimi.it>
2024-12-19 17:37:26 -0500

    b

--- compute_cell_QSO_new.py
+++ compute_cell_QSO_new.py
@@ -265,6 +265,7 @@ if data_type == 'data':
         print(np.sum(numcounts_map_N[numcounts_map_N>0]))
         print(np.sum(numcounts_map_S[numcounts_map_S>0]) + np.sum(numcounts_map_N[numcounts_map_N>0]))
         print(np.sum(numcounts_map[numcounts_map>0]))
+        print((np.sum(numcounts_map_S[numcounts_map_S>0]) + np.sum(numcounts_map_N[numcounts_map_N>0])/np.sum(ran_map)))
 
 
         """plt.figure()
