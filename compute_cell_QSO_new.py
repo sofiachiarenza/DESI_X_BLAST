f219c4f7724dd49cdc7d1056cfd7df13faa664b8
Sofia Chiarenza <sofia.chiarenza@studenti.unimi.it>
2024-12-19 18:16:14 -0500

    bvgfcd

--- compute_cell_QSO_new.py
+++ compute_cell_QSO_new.py
@@ -259,7 +259,7 @@ if data_type == 'data':
 
         print("SHOTNOISE TEST\n")
         print("ratio:\t", np.sum(masked_count_dn[bin_mask])/np.sum(completeness))
-        print("pixel area:\t", hp.pixelfunc.nside2pixarea(nside))
+        print("pixel area:\t", hp.pixelfunc.nside2pixarea(nside, degrees=True))
         print("number counts:\t", np.sum(masked_count_dn[bin_mask])/(np.sum(completeness)*hp.pixelfunc.nside2pixarea(nside)))
 
         """plt.figure()
