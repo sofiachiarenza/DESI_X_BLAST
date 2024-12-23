eae0982017ce3439c113079c5abd177c1f0d35dc
Sofia Chiarenza <sofia.chiarenza@studenti.unimi.it>
2024-12-23 23:06:55 +0100

    wrong indent

--- compute_cell_QSO_new.py
+++ compute_cell_QSO_new.py
@@ -258,20 +258,21 @@ if data_type == 'data':
             completeness = ran_map / ran_mean_S_DES
         else:
             completeness = ran_map / ran_mean_S
-        completeness = completeness * bin_mask.astype(np.float64)
-        completeness[completeness < cut_off] = 0
+        
+    completeness = completeness * bin_mask.astype(np.float64)
+    completeness[completeness < cut_off] = 0
 
-        """plt.figure()
-        hp.mollview(bin_mask, title='bin_mask')
-        plt.savefig(f'results/plots/bin_mask{opt3}.pdf')
+    """plt.figure()
+    hp.mollview(bin_mask, title='bin_mask')
+    plt.savefig(f'results/plots/bin_mask{opt3}.pdf')
         
-        plt.figure()
-        hp.mollview(completeness, title='comp_mask')
-        plt.savefig(f'results/plots/comp_mask{opt3}.pdf')
+    plt.figure()
+    hp.mollview(completeness, title='comp_mask')
+    plt.savefig(f'results/plots/comp_mask{opt3}.pdf')
         
-        plt.figure()
-        plt.hist(completeness[completeness > 0], bins=100)
-        plt.savefig(f'results/plots/comp_hist{opt3}.pdf')"""
+    plt.figure()
+    plt.hist(completeness[completeness > 0], bins=100)
+    plt.savefig(f'results/plots/comp_hist{opt3}.pdf')"""
 
 
     #use the numbercount maps to compute deltas
