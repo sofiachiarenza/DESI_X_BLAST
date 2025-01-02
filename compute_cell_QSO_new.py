a154f603d5529f33c0bee74b09bf5e6ec90b0cf4
Sofia Chiarenza <sofia.chiarenza@studenti.unimi.it>
2025-01-02 18:10:36 +0100

    aaaaarghhh

--- compute_cell_QSO_new.py
+++ compute_cell_QSO_new.py
@@ -165,10 +165,10 @@ if data_type == 'data':
         ran_mean_S_SGCnoDES = np.loadtxt(f'{PATH_d}QSO_z{zmin:.2f}_{zmax:.2f}_S-SGC-noDES__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_ran_mean.txt') # doesn't dep on completeness
         completeness_S_SGCnoDES = ran_map_S_SGCnoDES/ran_mean_S_SGCnoDES
         #STEP 3: S-NGCnoDES
-        masked_count_dn_S_NGCnoDES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S-NGC-noDES__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_DELTA_MAP.fits') #Directly read delta_map
-        bin_mask_S_NGCnoDES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S-NGC-noDES__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_BINARY_MASK.fits') #
-        ran_map_S_NGCnoDES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S-NGC-noDES__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_RAN_MAP.fits') # why not exactly same dep completeness?
-        ran_mean_S_NGCnoDES = np.loadtxt(f'{PATH_d}QSO_z{zmin:.2f}_{zmax:.2f}_S-NGC-noDES__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_ran_mean.txt') # doesn't dep on completeness
+        masked_count_dn_S_NGCnoDES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S-NGC__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_DELTA_MAP.fits') #Directly read delta_map
+        bin_mask_S_NGCnoDES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S-NGC__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_BINARY_MASK.fits') #
+        ran_map_S_NGCnoDES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S-NGC__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_RAN_MAP.fits') # why not exactly same dep completeness?
+        ran_mean_S_NGCnoDES = np.loadtxt(f'{PATH_d}QSO_z{zmin:.2f}_{zmax:.2f}_S-NGC__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_ran_mean.txt') # doesn't dep on completeness
         completeness_S_NGCnoDES = ran_map_S_NGCnoDES/ran_mean_S_NGCnoDES
 
         #handle overlapping pixels
@@ -201,7 +201,7 @@ if data_type == 'data':
         numcounts_map_S = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_DATA_MAP.fits', field=[0])
         if extrasepnorm:
             numcounts_map_S_DES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_DES__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_DATA_MAP.fits', field=[0])
-            numcounts_map_S_NGCnoDES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S-NGC-noDES__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_DATA_MAP.fits', field=[0])
+            numcounts_map_S_NGCnoDES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S-NGC__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_DATA_MAP.fits', field=[0])
             numcounts_map_S_SGCnoDES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S-SGC-noDES__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_DATA_MAP.fits', field=[0])
             numcounts_map_S = numcounts_map_S_DES + numcounts_map_S_NGCnoDES + numcounts_map_S_SGCnoDES
 
@@ -210,7 +210,7 @@ if data_type == 'data':
         numcounts_map_S = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_DATA_MAP_UNW.fits', field=[0])
         if extrasepnorm:
             numcounts_map_S_DES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_DES__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_DATA_MAP_UNW.fits', field=[0])
-            numcounts_map_S_NGCnoDES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S-NGC-noDES__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_DATA_MAP_UNW.fits', field=[0])
+            numcounts_map_S_NGCnoDES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S-NGC__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_DATA_MAP_UNW.fits', field=[0])
             numcounts_map_S_SGCnoDES = hp.read_map(f'{PATH_d}/QSO_z{zmin:.2f}_{zmax:.2f}_S-SGC-noDES__HPmapcut_{w_str}_nside2048_{version}_comp{comp}_galactic_DATA_MAP_UNW.fits', field=[0])
             numcounts_map_S = numcounts_map_S_DES + numcounts_map_S_NGCnoDES + numcounts_map_S_SGCnoDES
 
